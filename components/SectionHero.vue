f902c8516d06cab4f75327e270b3a8b2aebb6236
Nass Donald <nassdonald@users.noreply.github.com>
2020-09-15 03:50:41 +0100

    hero tweaks

--- components/SectionHero.vue
+++ components/SectionHero.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="section-hero">
+    <big-v class="letter" />
     <div class="tm-section-container section-container">
       <div class="container">
         <nav class="nav-primary">
@@ -9,14 +10,11 @@
             </li>
           </ul>
           <div class="headings">
-            <logo-hackatom-brandmark-color class="brandmark" />
             <wordmark-hackatom class="wordmark" />
-            <span class="sr-only">HackAtom</span>
+            <span class="sr-only">HackAtom V</span>
+            <logo-hackatom-brandmark-color class="brandmark" />
           </div>
         </nav>
-        <!-- TODO: svg is broken -->
-        <!-- <big-v class="letter" /> -->
-        <!-- <img src="/V.svg" /> -->
         <div class="hero-bottom">
           <div class="hero-bottom__subtitle tm-rf2 tm-lh-title">
             Strap on your spacepants and get ready to hack, Cosmonaut.
@@ -30,7 +28,9 @@
             variant="contained"
             glow
             class="hero-btn"
-            >Apply<span class="icon__right">--></span></tm-button
+            >Apply<span class="icon__right" aria-hidden="true"
+              >--></span
+            ></tm-button
           >
         </div>
       </div>
@@ -45,44 +45,46 @@ export default {}
 <style lang="stylus" scoped>
 // Accessible/SEO friendly CSS hiding
 .sr-only
-  position absolute
-  height 1px
-  width 1px
-  overflow hidden
-  clip rect(1px, 1px, 1px, 1px)
+  clip()
 
 .section-container
   position relative
-  max-width 96rem
+  max-width $max-width-11
   margin 0 auto
   .container
     display grid
     grid-template-columns repeat(12, 1fr)
-    gap 0 2rem
+    gap 0 var(--spacing-7)
     max-width var(--container-mw)
     margin 0 auto
 
 .section-hero
   position relative
   height 100vh
-  min-height 52rem
-  max-height 64rem
-  margin-bottom 6rem
+  min-height 50rem
+  max-height 54rem
+  max-width $max-width-11
+  margin 0 auto var(--spacing-10)
   display flex
   justify-content stretch
   align-items stretch
   text-align center
   background-image url('/hero-bg.jpg')
-  background-position center center
+  background-position top center
   background-repeat no-repeat
   background-size cover
-  // max-width 92.5rem
+  .letter
+    position absolute
+    width 100%
+    height auto
+    max-height 26rem
+    top 48%
   .section-container
     display flex
     align-items stretch
     width 100%
-    padding-top 4rem
-    padding-bottom 6rem
+    padding-top var(--spacing-8)
+    padding-bottom var(--spacing-8)
   .container
     position relative
     display flex
@@ -94,17 +96,20 @@ export default {}
       margin-bottom 5%
     .headings
       position relative
-      margin-top 5rem
-      svg
+      max-width 54rem
+      margin calc(1.5rem + 4vh) auto 0
+      .brandmark
+        $w = (200 / (54 * 16)) * 100% // intrinsic / (max-width * 1rem) * 100%
+        position absolute
+        top -68%
+        height auto
+        width $w
+        left ((100% - $w)/2)
+      .wordmark
+        position relative
         width 100%
         height auto
         max-height 7.1875rem
-      .brandmark
-        position absolute
-        top -0.5rem
-        height 90%
-      .wordmark
-        position relative
 
 .nav-primary
   ul
@@ -121,24 +126,25 @@ export default {}
     margin 0 auto
 
 .hero-bottom
-  max-width 30.375rem
+  max-width $max-width-7
   margin 0 auto
   &__subtitle
-    margin-bottom 3rem
+    margin-bottom var(--spacing-8)
     color var(--white)
 
-@media screen and (max-width: 576px)
+@media $breakpoint-xsmall-only
   .section-hero
     min-height 32rem
     max-height 42rem
-    margin-bottom 3rem
     .section-container
-      padding-top 0.75rem
-      padding-bottom 2rem
+      background-image linear-gradient(to bottom, rgba(28, 7, 1, 0.90) 0%, rgba(80, 25, 13, 0) 30%)
+      padding-top var(--spacing-4)
+      padding-bottom var(--spacing-9)
     .container
       .nav-primary
         margin-bottom 3rem
       .headings
+        margin-top calc(3rem + 4vh)
         .title
           padding-left 1rem
           padding-right 1rem
