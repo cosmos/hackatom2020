15cdd8db9d88c80d54fa4c2ad3adb98932a659d2
Nass Donald <nassdonald@users.noreply.github.com>
2020-09-15 17:12:10 +0100

    fixed footer (wip)

--- components/SectionFooter.vue
+++ components/SectionFooter.vue
@@ -1,38 +1,40 @@
 <template>
   <footer>
-    <div class="container tm-section-container">
-      <div class="top-logo">
-        <logo-footer-wordmark />
+    <div class="tm-section-container">
+      <div class="container">
+        <div class="top-logo">
+          <logo-footer-wordmark />
+        </div>
+        <div class="nav tm-rf0 tm-medium">
+          <a
+            href="https://hackatomv.devpost.com"
+            target="blank_"
+            rel="noreferrer noopener"
+            class="cta"
+            >Apply</a
+          >
+          <a
+            href="https://discord.com/channels/669268347736686612/746658605213810779"
+            target="blank_"
+            rel="noreferrer noopener"
+            class="cta"
+            >Chat</a
+          >
+        </div>
+        <div class="blockquote tm-rf-1 tm-rf0-m-up tm-lh-copy">
+          *$50K USD valued in ATOMs. The spot price of ATOM will be taken on the
+          last day of the hackathon, on Oct 30th, where the day’s ‘High’ value
+          will be used.
+        </div>
+        <div class="divider"></div>
+        <div class="smallprint tm-rf-1 tm-rf0-m-up tm-lh-copy">
+          This website is maintained by Tendermint Inc. The contents and
+          opinions of this website are those of Tendermint Inc.
+        </div>
       </div>
-      <div class="nav tm-rf0 tm-medium">
-        <a
-          href="https://hackatomv.devpost.com"
-          target="blank_"
-          rel="noreferrer noopener"
-          class="cta"
-          >Apply</a
-        >
-        <a
-          href="https://discord.com/channels/669268347736686612/746658605213810779"
-          target="blank_"
-          rel="noreferrer noopener"
-          class="cta"
-          >Chat</a
-        >
+      <div class="bot-logo">
+        <logo-hackatom-brandmark />
       </div>
-      <div class="blockquote tm-rf0 tm-lh-copy">
-        *$50K USD valued in ATOMs. The spot price of ATOM will be taken on the
-        last day of the hackathon, on Oct 30th, where the day’s ‘High’ value
-        will be used.
-      </div>
-      <div class="divider"></div>
-      <div class="smallprint tm-rf0 tm-lh-copy">
-        This website is maintained by Tendermint Inc. The contents and opinions
-        of this website are those of Tendermint Inc.
-      </div>
-    </div>
-    <div class="bot-logo">
-      <logo-hackatom-brandmark />
     </div>
   </footer>
 </template>
@@ -43,26 +45,32 @@ export default {}
 
 <style lang="stylus" scoped>
 footer
-  display flex
-  justify-content center
-  align-items center
-  text-align center
-  color var(--white)
-  background-image url('/bg/rocket-launch.jpg'), linear-gradient(90deg, #0D0D0D 0.03%, rgba(13, 13, 13, 0) 20.85%, rgba(13, 13, 13, 0) 80.19%, #0D0D0D 99.97%), linear-gradient(180deg, #0D0D0D 3.38%, rgba(13, 13, 13, 0) 54.44%)
-  background-position center center
+  background-image url('/bg/rocket-launch.jpg')
+  background-position bottom center
   background-repeat no-repeat
   background-size cover
+  margin var(--spacing-9) auto 0
+  max-width $max-width-11
+
+.tm-section-container
+  display flex
+  flex-direction column
+  align-items center
   position relative
 
 .container
-  max-width 64rem
-  margin-bottom 26.125rem
-  width 100%
+  text-align center
+  max-width $max-width-9
+  margin 0 auto
 
 .nav
-  padding 3rem 0
+  padding var(--spacing-8) 0
   display flex
   justify-content center
+  a
+    &:hover,
+    &:focus
+      color var(--primary-600)
 
 .cta
   &:not(:last-child):after
@@ -77,21 +85,20 @@ footer
   color var(--white-700)
 
 .divider
-  border 1px solid rgba(255, 255, 255, 0.333)
-  width 2.5rem
+  border 1px solid var(--white-300)
+  width var(--spacing-7)
   display inline-block
-  margin 1.5rem 0
+  margin var(--spacing-6) 0
 
 .bot-logo
-  position absolute
-  right 8.6875rem
-  bottom 2rem
+  padding var(--spacing-13) 0 var(--spacing-9)
+  display grid
+  place-items center
 
-@media screen and (max-width: 600px)
+@media $breakpoint-medium
+  footer
+    background-size contain
+
   .bot-logo
-    position absolute
-    right unset
-    bottom 2rem
-    display grid
-    place-items center
+    align-self flex-end
 </style>
