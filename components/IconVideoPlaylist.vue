dff50ec455915854aa6895c763dbcde345a25d07
Cyrus Goh <hello@lovincyrus.com>
2020-09-16 15:51:10 -0700

    video playlist icon for section workshops

---
+++ components/IconVideoPlaylist.vue
@@ -0,0 +1,28 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M3.5 6.5H21.5C22.0304 6.5 22.5391 6.71071 22.9142 7.08579C23.2893 7.46086 23.5 7.96957 23.5 8.5V20.5C23.5 21.0304 23.2893 21.5391 22.9142 21.9142C22.5391 22.2893 22.0304 22.5 21.5 22.5H3.5C2.96957 22.5 2.46086 22.2893 2.08579 21.9142C1.71071 21.5391 1.5 21.0304 1.5 20.5V8.5C1.5 7.96957 1.71071 7.46086 2.08579 7.08579C2.46086 6.71071 2.96957 6.5 3.5 6.5V6.5Z"
+      stroke="#E96C58"
+      stroke-miterlimit="10"
+      stroke-linecap="square"
+    />
+    <path
+      d="M10.5 11.5L15.5 14.5L10.5 17.5V11.5Z"
+      stroke="#E96C58"
+      stroke-miterlimit="10"
+      stroke-linecap="square"
+    />
+    <path
+      d="M19.5 2.5H5.5"
+      stroke="#E96C58"
+      stroke-miterlimit="10"
+      stroke-linecap="square"
+    />
+  </svg>
+</template>
