f5d26e458239fcd24565c2d9333526d2833e614c
Cyrus Goh <hello@lovincyrus.com>
2020-09-18 22:32:28 -0700

    remove purgecss

--- nuxt.config.js
+++ nuxt.config.js
@@ -151,8 +151,6 @@ export default {
     '@aceforth/nuxt-optimized-images',
     // https://github.com/nuxt-community/style-resources-module
     '@nuxtjs/style-resources',
-    // https://github.com/Developmint/nuxt-purgecss
-    'nuxt-purgecss',
   ],
   optimizedImages: {
     optimizeImages: true,
